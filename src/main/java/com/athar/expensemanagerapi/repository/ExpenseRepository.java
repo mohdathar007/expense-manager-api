034befb393b6352e68a8e5bcafe4199eafa14b1d
Athar <mohdathar8888@gmail.com>
2024-04-18 19:03:19 +0530

    uploaded expense-manager-api

---
+++ src/main/java/com/athar/expensemanagerapi/repository/ExpenseRepository.java
@@ -0,0 +1,27 @@
+package com.athar.expensemanagerapi.repository;
+
+
+
+import java.sql.Date;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.athar.expensemanagerapi.entity.Expense;
+
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+
+    Page<Expense> findByCategory( String category, Pageable page);
+
+    Page<Expense> findByNameContaining( String keyword, Pageable page);
+
+
+
+
+
+}
---
+++ src/main/java/com/athar/expensemanagerapi/service/ExpenseService.java
@@ -0,0 +1,30 @@
+package com.athar.expensemanagerapi.service;
+
+
+
+import java.sql.Date;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import com.athar.expensemanagerapi.entity.Expense;
+
+
+public interface ExpenseService {
+
+    Page<Expense> getAllExpenses(Pageable page);
+
+    Expense getExpenseById(Long id);
+
+    void deleteExpenseById(Long id);
+
+    Expense saveExpenseDetails(Expense expense);
+
+    Expense updateExpenseDetails(Long id, Expense expense);
+
+    List<Expense> readByCategory(String category, Pageable page);
+
+    List<Expense> readByName(String keyword, Pageable page);
+
+
+}
